14ceae105f5787b713bc4c117002353ea34e6ab2
peterlimo <kiproplimo8@gmail.com>
2022-02-09 05:28:54 +0300

    lll

---
+++ app/src/main/java/com/heron/constructmanager/Constants.java
@@ -0,0 +1,41 @@
+package com.heron.constructmanager;
+
+public class Constants {
+    public static final String ACC_VERIFIED = "Verified account";
+    public static final String CONTRACTOR = "Contractor";
+    public static final String EMAIL_ALREADY_VERIFIED = "Email has already been verified.";
+    public static final String EMAIL_INVALID = "Invalid email.";
+    public static final String EMAIL_UPDATED = "Email successfully updated.";
+    public static final String EMAIL_VERIF_SEND = "Verification email sent.";
+    public static final String PLEASE_LOGIN =  "Please log in to use the app.";
+    public static final String UNEXPECTED_ERROR = "Unexpected error. Try again.";
+    public static final String NEW_STAGE = "Step Forward";
+    public static final String CANCEL = "Cancellation";
+
+    public static final String SUCCESS = " successfully carried out.";
+    public static final String FAILURE = "Error ";
+
+    // CRUD Operations
+    public static final String WRITE = "Register";
+    public static final String DELETE = "Removed";
+    public static final String UPDATE = "Update";
+
+    // Construction Stages
+    public static final String CANCELLED = "Canceled";
+    public static final String FINISHED = "Completed";
+    public static final String EXEC = "Execution";
+    public static final String PREP = "Preparation";
+
+    // Schedule States
+    public static final String ON_SCHEDULE = "On time";
+    public static final String LATE = "Late";
+    public static final String SOLVED = "Solved";
+
+    // Responsability states
+    public static final String OPEN = "Open";
+
+
+
+
+
+}
